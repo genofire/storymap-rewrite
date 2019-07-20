45558d2a44dabf02753b85188238cd372242d573
Janne Jensen <janne.jensen@posteo.de>
2019-07-20 03:24:03 +0200

    first commit

---
+++ js/components/storymap-rewrite.js
@@ -0,0 +1,336 @@
+// Modified by Bo Zhao, zhao2@oregonstate.edu
+// Originally obtained from http://atlefren.github.io/storymap/
+// Updated on 05/04/2018 | version 2.4.0 | MIT License
+
+(function($) {
+
+  $.fn.storymap = function(options) {
+
+    var defaults = {
+      selector: '[data-scene]',
+      triggerpos: '30%',
+      navwidget: false,
+      legend: true,
+      loader: true,
+      flyto: false,
+      scalebar: false,
+      progressline: true,
+      dragging: true,
+      mapinteraction: false,
+      zoomControl: false
+    };
+
+    var settings = $.extend(defaults, options);
+
+    if (typeof(L) === 'undefined') {
+      throw new Error('Storymap requires Leaflet.');
+    }
+
+    // Define Scene 1 as Opening Scene
+    var currentScene = "scene1";
+
+    function getFirstSection(sections) {
+
+      var thesections = $.map(sections, function(element) {
+        return {
+          el: $(element)
+        };
+      });
+
+      $.each(sections, function(key, element) {
+
+        var section = $(element);
+        if (section[0].dataset.scene !== currentScene) {
+          section.trigger('notviewing');
+        } else {
+          section.trigger('viewing');
+        };
+      });
+    }
+
+    // watchHighlight rewrite
+    function showSection(element, searchfor) {
+      var sections = element.find(searchfor);
+      getFirstSection(sections);
+    }
+
+    //support video for IE 8 and 9.
+    document.createElement('video');
+
+    var makeStoryMap = function(element, scenes) {
+
+      $(element).addClass("storymap");
+      /*
+      var topElem = $('<div class="storymap-trigger"></div>')
+        .css('top', settings.triggerpos);
+      $('body').append(topElem);
+      var top = topElem.offset().top - $(window).scrollTop();
+      */
+      var searchfor = settings.selector;
+      var sections = $(element).find(searchfor);
+      // console.log(sections);
+      var map = settings.createMap();
+
+
+      var currentLayerGroup = L.layerGroup().addTo(map);
+      // var nav = $("nav");
+
+
+      if (settings.baselayer) {
+        // add a base map, which can be either OSM, mapbox, tilelayer, wmslayer or those designed by yourself.
+        settings.baselayer.layer.addTo(map);
+      }
+
+      if (settings.legend) {
+        $(".storymap").append("<div class='storymap-legend' />")
+      }
+
+      if (settings.scalebar) {
+        L.control.scale({
+          position: "bottomright",
+          metric: true,
+          imperial: false
+        }).addTo(map);
+      }
+
+      if (settings.zoomControl) {
+        L.control.zoom({
+          position:'bottomright'
+        }).addTo(map);
+      }
+
+      if (settings.progressline) {
+        $(".storymap").append("<div class='storymap-progressline' />")
+
+      }
+
+      if (settings.navwidget) {
+        $(".storymap").append("<div class='storymap-navwidget text-center'/>")
+
+      }
+
+      if (settings.loader) {
+        $(".storymap").append("<div class='icon ion-md-refresh storymap-loader'></div>")
+
+      }
+
+
+      $(".storymap-map .leaflet-control-attribution")
+        .addClass("storymap-attribution")
+        .html("<a href='https://github.com/jakobzhao/storymap'><img src='https://jakobzhao.github.io/storymap/img/logo.png' width='18px' target='_blank' > storymap.js </a>");
+
+
+      if (settings.credits) {
+        $(".storymap-attribution").find("a").prepend(settings.credits + " | ");
+
+      }
+
+      if (settings.dragging) {
+
+        map.dragging.enable();
+        if (map.tap) map.tap.enable();
+        $("storymap-map").css("cursor", "grab");
+
+      } else {
+
+        map.dragging.disable();
+        if (map.tap) map.tap.disable();
+        $("storymap-map").css("cursor", "pointer");
+      }
+
+      if (settings.mapinteraction) {
+
+        map.touchZoom.enable();
+        map.doubleClickZoom.enable();
+        map.scrollWheelZoom.enable();
+        map.boxZoom.enable();
+        map.keyboard.enable();
+        if (map.tap) map.tap.enable();
+
+        $("storymap-map").css("cursor", "grab");
+
+      } else {
+
+        map.touchZoom.disable();
+        map.doubleClickZoom.disable();
+        map.scrollWheelZoom.disable();
+        map.boxZoom.disable();
+        map.keyboard.disable();
+        if (map.tap) map.tap.disable();
+        $("storymap-map").css("cursor", "pointer");
+      }
+
+      /*
+      $.each(sections, function(key, element) {
+        var section = $(element);
+
+        var path = section.data('background');
+        console.log(section);
+        if (typeof path !== 'undefined') {
+
+
+          if (path.indexOf("jpg") >= 0 || path.indexOf("jpeg") >= 0 || path.indexOf("png") >= 0 || path.indexOf("bmp") >= 0 || path.indexOf("gif") >= 0) {
+            $("head").append("<style> ." + section.data('scene') + "-bg-img { background: url(" + path + ") no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover;  -o-background-size: cover; background-size: cover; }</style>");
+
+            $(section).find(".fullscreen").addClass(section.data('scene') + "-bg-img");
+
+          } else if (path.indexOf("mp4") >= 0) {
+
+            $(section).find(".fullscreen").before('<video class="fullscreen canvas-center"  playsinline autoplay muted loop><source src=' + path + '  type="video/mp4"></video>')
+
+          } else {
+            console.log(path);
+          }
+        }
+
+      });
+      */
+
+
+      if (!String.prototype.includes) {
+        String.prototype.includes = function() {
+          'use strict';
+          return String.prototype.indexOf.apply(this, arguments) !== -1;
+        };
+      }
+
+      $.each(layers, function(key, layer) {
+
+        // layer = layer.layer;
+        layer.layer.on('s');
+        layer.layer.on('load', function() {
+          $(".storymap-loader").fadeTo(1000, 0);
+        })
+
+      });
+
+      function showMap(key) {
+
+        currentLayerGroup.clearLayers();
+
+        var scene = scenes[key];
+        var legendContent = "";
+
+        if (typeof $("section[data-scene='" + key + "']").data("background") !== 'undefined') {
+
+          $(".storymap-loader").fadeTo(0, 0);
+
+        } else if (typeof $("section[data-scene='" + key + "']").data("background") === 'undefined') {
+
+          for (var i = 0; i < scene.layers.length; i++) {
+            $(".storymap-loader").fadeTo(0, 1);
+            currentLayerGroup.addLayer(scene.layers[i].layer);
+
+            if (typeof scene.layers[i].legend !== 'undefined') {
+              legendContent += scene.layers[i].legend;
+            }
+          }
+          $(".storymap-loader").fadeTo(1, 0);
+
+        }
+
+        // the condition legendContent != "" will make sure the legend will only be added on when there is some contents in the legend.
+        if (settings.legend && legendContent !== "") {
+          $(".storymap-legend")
+            .html(legendContent)
+            .show();
+        } else {
+          $(".storymap-legend").hide();
+
+        }
+
+        if (settings.flyto) {
+          map.flyTo([scene.lat, scene.lng], scene.zoom, {
+            animate: true,
+            easeLinearity: 0.2,
+            duration: 4 // in seconds
+          })
+        } else {
+          map.setView([scene.lat, scene.lng], scene.zoom, {
+            animate: false,
+            easeLinearity: 0.2,
+            duration: 4 // in seconds
+          })
+
+        }
+
+        map.invalidateSize();
+
+      }
+
+      function executeScript(key) {
+        var scene = scenes[key];
+        var section = $('section[data-scene="' + key + '"]')
+        console.log(section);
+        scene.script(key, map, section);
+      }
+
+
+      sections.on('viewing', function() {
+
+        $(this)
+          //.removeClass('invisible')
+          .removeClass('hide')
+          .addClass('visible')
+          .addClass('viewing')
+          .addClass('show');
+
+        if (typeof $(this).data("background") !== 'undefined') {
+          $(this)
+            .addClass('section-opacity');
+        }
+
+        // Show corresponding Data Scene
+        showMap($(this).data('scene'));
+
+        if (typeof(scenes[$(this).data('scene')].script) !== 'undefined') {
+          executeScript($(this).data('scene'));
+        }
+      });
+
+      sections.on('notviewing', function() {
+
+        $(this)
+          .removeClass('viewing')
+          .removeClass('show')
+          .removeClass('visible')
+          // .addClass('invisible')
+          .addClass('hide');
+      });
+
+      showSection(element, searchfor);
+
+      // Sidemenu Scene Switch
+      var links = $('a[href^="#scene"]');
+      links.each(function(index) {
+
+        $(this).on("click", function() {
+
+          if (currentScene !== this.dataset.target) {
+            currentScene = this.dataset.target;
+            showSection(element, searchfor);
+          };
+
+          $('#sidebar').toggleClass('active');
+          $('.overlay').removeClass('active');
+        });
+        
+      });
+
+      // create a progress line
+      $(window).scroll(function() {
+        var wintop = $(window).scrollTop(),
+          docheight = $(document).height(),
+          winheight = $(window).height();
+        var scrolled = (wintop / (docheight - winheight)) * 100;
+
+        $('.storymap-progressline').css('width', (scrolled + '%'));
+      });
+
+    };
+
+    makeStoryMap(this, settings.scenes);
+    return this;
+  }
+
+}(jQuery));
---
+++ js/main.js
@@ -0,0 +1,183 @@
+
+// Layer Styling
+const data_route_style = {
+  "color": '#706d93', // $rhythm
+  "weight": 5,
+  "opacity": 0.7,
+	"lineCap": 'round',
+	"dashArray": "0.7 12"
+};
+
+const data_ports = {
+  "color": '#209488',
+  "weight": 2,
+  "fillColor": "#209488",
+  "opacity": 0.7,
+  "fillOpacity": 0.7
+}
+
+// Setup leaflet layers
+var layers = {
+  carto_positron: {
+    layer: L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 19
+    })
+  },
+  stamen_toner_bg: {
+    layer: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.{ext}', {
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      subdomains: 'abcd',
+      minZoom: 0,
+      maxZoom: 20,
+      ext: 'png'
+    })
+  },
+  stamen_terrain: {
+    layer: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      subdomains: 'abcd',
+      minZoom: 0,
+      maxZoom: 20,
+      ext: 'png'
+    })
+  },
+  esri_world: {
+    layer: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    })
+  },
+  data_points: {
+    layer: L.geoJson.ajax('./data/points.geojson'),
+  },
+  data_route: {
+    layer: L.geoJson.ajax('./data/route.geojson', {
+      style: data_route_style
+    }),
+  },
+  weserkorrektion: {
+    layer: L.tileLayer('./data/05_port_construction/Weserkorrektion_tiles/{z}/{x}/{y}.png', {
+      tms: true,
+      opacity: 0.8,
+      attribution: ""
+    })
+  },
+  data_ports_1882: {
+    layer: L.geoJson.ajax('./data/04_ueberseehafenbecken/ports_1882+4326.geojson', {
+      style: data_ports
+    }),
+  },
+  data_ports_1884: {
+    layer: L.geoJson.ajax('./data/04_ueberseehafenbecken/ports_1884+4326.geojson', {
+      style: data_ports
+    }),
+  },
+  data_ports_1914: {
+    layer: L.geoJson.ajax('./data/04_ueberseehafenbecken/ports_1914+4326.geojson', {
+      style: data_ports
+    }),
+  },
+};
+
+// Setup Scenes/Chapters
+var scenes = {
+  overview: {
+    lat: 53.09460389460539,
+    lng: 8.771724700927736,
+    zoom: 16,
+    layers: [layers.data_route, layers.data_points],
+    name: "Overview"
+  },
+  scene1: {
+    lat: 53.09460389460539,
+    lng: 8.771724700927736,
+    zoom: 16,
+    layers: [layers.data_route, layers.data_points],
+    name: "Introduction"
+  },
+  scene2: {
+    lat: 53.3057,
+    lng: 8.7362,
+    zoom: 10,
+    layers: [layers.esri_world, layers.weserkorrektion],
+    name: "River Conservancy",
+  },
+  scene3: {
+    lat: 53.11952960361747,
+    lng: 8.729152679443361,
+    zoom: 14,
+    layers: [layers.esri_world, layers.weserkorrektion],
+    name: "Durchstich der Langen Bucht",
+  },
+  scene4: {
+    lat: 53.098778508646554,
+    lng: 8.764278888702394,
+    zoom: 16,
+    layers: [layers.data_ports_1882],
+    name: "Harbor Basins (1882)",
+  },
+  scene5: {
+    lat: 53.1036097321532,
+    lng: 8.758850097656252,
+    zoom: 15,
+    layers: [layers.data_ports_1884],
+    name: "Harbor Basins (1884)",
+  },
+  scene6: {
+    lat: 53.098778508646554,
+    lng: 8.764278888702394,
+    zoom: 15,
+    layers: [layers.data_ports_1914],
+    name: "Harbor Basins (1914)",
+  }
+};
+
+// Create Map
+var createMap = function() {
+  var map = L.map($('.storymap-map')[0], {
+    zoomControl: false
+  }).setView([44, -120], 7);
+
+  console.log('storymap.js createMap');
+  // L.tileLayer('http://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}@2x.png').addTo(map);
+  return map;
+}
+
+$('#storymap').storymap({
+  scenes: scenes,
+  baselayer: layers.carto_positron,
+  legend: true,
+  navbar: false,
+  loader: true,
+  flyto: true,
+  dragging: true,
+  credits: "<i class='icon ion-md-build' style='font-size: 10px;'></i> with <i class='icon ion-md-heart' style='color: red; font-size: 10px;'></i> from Bo Zhao",
+  scalebar: true,
+  scrolldown: false,
+  progressline: true,
+  zoomControl: true,
+  createMap: createMap,
+});
+
+// Sidemenu Toggle
+$('#dismiss, .overlay').on('click', function () {
+            // hide sidebar
+            $('#sidebar').removeClass('active');
+            $('.overlay').removeClass('active');
+        });
+
+$('#sidebarCollapse').click(function() {
+  $('#sidebar').toggleClass('active');
+	$('.overlay').addClass('active');
+});
+
+// Content Toggle
+function toggleIcon(e) {
+  $("#contentCollapse")
+    .find(".icon")
+    .toggleClass('ion-md-arrow-dropup ion-md-arrow-dropdown');
+}
+
+$('.section-content').on('hidden.bs.collapse', toggleIcon);
+$('.section-content').on('show.bs.collapse', toggleIcon);
